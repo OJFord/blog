6acfce4610488d65330cdfcddeb5c2abebe29317
Ollie Ford <me@ojford.com>
2017-01-14 04:03:33 +0000

    Add bucket access policy

--- infrastructure.tf
+++ infrastructure.tf
@@ -2,6 +2,21 @@ resource "aws_s3_bucket" "blog" {
   bucket = "${var.s3_bucket}"
   acl    = "public-read"
 
+  policy = <<EOF
+{
+    "Version":"2012-10-17",
+    "Statement":[
+        {
+            "Sid":"PublicReadGetObject",
+            "Effect":"Allow",
+            "Principal": "*",
+            "Action":["s3:GetObject"],
+            "Resource":["arn:aws:s3:::${var.s3_bucket}/*"]
+        }
+    ]
+}
+EOF
+
   website = {
     index_document = "index.html"
   }
