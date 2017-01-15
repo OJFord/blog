8062b34f8256d3076e2ef01d6a24edaeaccb32a6
Ollie Ford <me@ojford.com>
2017-01-15 01:44:39 +0000

    Allow CORS of S3 bucket

--- infrastructure.tf
+++ infrastructure.tf
@@ -17,6 +17,14 @@ resource "aws_s3_bucket" "blog" {
 }
 EOF
 
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://${var.domain}", "https://*.${var.domain}"]
+    expose_headers = ["ETag"]
+    max_age_seconds = 3000
+  }
+
   website = {
     index_document = "index.html"
   }
